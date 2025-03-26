5f1bfda30957ba867ec0b74965faacc1813019dd
MANISH KUMAR <manishkumar970034@gmail.com>
2025-03-26 23:36:14 +0530

    final

--- src/aichef.js
+++ src/aichef.js
@@ -1,11 +1,9 @@
 
 import { HfInference } from '@huggingface/inference'
-import dotenv from 'dotenv'
-dotenv.config();
 
 const SYSTEM_PROMPT = "You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page"
 
-const API_KEY = process.env.API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const hf = new HfInference(API_KEY);
 
